36bb0c8f17cb5e7ae2504df02e9a1043e71209dc
Shannon Kuehn <shannon.kuehn@gmail.com>
2022-04-26 13:32:36 -0500

    Update schedRolloverKerbKeys.ps1

--- schedRolloverKerbKeys.ps1
+++ schedRolloverKerbKeys.ps1
@@ -1,5 +1,5 @@
 # The variable $CloudUser requires UPN of a Global Administrator user (username@companyname.com).
-# The variable $Onpremuser requires a Down-Level Logon Name (DOMAIN\USERNAME)
+# The variable $Onpremuser requires a Down-Level Logon Name (DOMAIN\USERNAME). The user can be a domain administrator or a hybrid identity administrator.
 
 $CloudUser = 'username@companyname.com'
 $CloudEncrypted = Get-Content "C:\Scripts\azureAd_Encrypted_Password.txt" | ConvertTo-SecureString
